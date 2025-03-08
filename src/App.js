461b109c8f6d550e40278039dd550cdaeac0bc5d
RutujaKhurpe <rutujakhurpe99@gmail.com>
2025-03-08 11:46:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "../src/components/Navbar.jsx"
+import Home from './pages/Home.jsx';
+function App() {
+  return (
+    <>
+    
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        logo
+      </div>
+      <div>
+home, about etc ect
+      </div>
+    </div>
+  )
+}
